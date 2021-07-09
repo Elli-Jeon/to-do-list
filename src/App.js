f0f530e771bf1bde069732dad62e7a2a4d5a0c97
raverena <raverena96@gmail.com>
2021-07-10 02:20:34 +0900

    [Update] Learn usecontext

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, createContext } from "react";
 import { ThemeProvider } from 'styled-components';
 // ThemeProvider : 반드시 theme 속성을 갖는 컴포넌트. theme 속성의 값으로 설정된 객체를 모든 하위 컴포넌트들의 props로 전달 (styled-components의 context라고 보면 될 듯)
 import { darkTheme, lightTheme } from "./styles/Theme";
 import GlobalStyle from "./styles/GlobalStyle";
 import Header from "./components/Header";
 import Container from "./components/Container";
+
+
+// context도 컴포넌트이니깐 컴포넌트 바깥에서 정의해서. 
+export const MyContext = createContext("H");
 
 const App = () => {
     const [ theme, setTheme ] = useState(lightTheme);
@@ -18,11 +22,14 @@ const App = () => {
         setCurrentThemeText(nextThemeText);
     };
 
+
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
-            <Header switchTheme={switchTheme} />
-            <Container currentThemeText={currentThemeText} />
+            <MyContext.Provider value = "Elli is Here!">
+                <Header switchTheme={switchTheme} />
+                <Container currentThemeText={currentThemeText} />
+            </ MyContext.Provider>
         </ThemeProvider>
     )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { MyContext } from '../App' // export 했으니 받아줘서
 import styledComponents from '../styles/Header-style'
-const { CustomHeader, ThemeSwitchBtn} = styledComponents;
+const { CustomHeader, ThemeSwitchBtn } = styledComponents;
 
 const Header = ({ switchTheme }) => {
+    const text = useContext(MyContext);
+    console.log(text);
+    
     return (
         <CustomHeader>
             <ThemeSwitchBtn onClick={()=>{
