599f6ee32bace0b81d18b560e895c698ab827bfe
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-07-13 22:13:35 +0900

    [Add] 삭제 버튼 누르면 돌아서 사라지는 효과

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,13 +12,21 @@ const Todo = ({todo}) => {
     console.log(todo);
     const context = useContext(TodoContext);
     const [ edit, setEdit ] = useState(false);
-    const [ newTodo, SetNewTodo ] = useState("");
+    const [ newTodo, SetNewTodo ] = useState();
+    const [ deleted, setDeleted ] = useState(false);
 
     const todoDelete = (e) => {
         e.preventDefault();
         //console.log(e);
-        context.dispatch({type : ACTIONS.TODO_DELETE, payload : { todo }});
+        setDeleted(true);
+        // setTimeout 안 걸면 바로 상태 변화로 상위 MainTodo에서 map돌 때 삭제되어 버림.
+        setTimeout(()=>{
+            context.dispatch({type : ACTIONS.TODO_DELETE, payload : { todo }});
+            setDeleted(false);
+        },3000)        
     }
+
+    // setTimeOut useEffect에 담아야 함.
 
     const todoEdit = (e) => {
         e.preventDefault();
@@ -32,9 +40,9 @@ const Todo = ({todo}) => {
             context.dispatch({type : ACTIONS.TODO_EDIT, payload : { todo, newTodo }})
         }     
     }
-
+            /*삭제 버튼 클릭하면 className 들어와서 className에 수동으로 들어가서, styled-components 부분에서 props.className으로 조회 */
     return (
-        <TodoItem>
+        <TodoItem className={deleted? 'deleted' : null}>
             <h2>{todo.priority}</h2> 
             {edit? (<input placeholder={todo.todo} onChange={todoEdit}/>) : (<p>{todo.todo}</p>)}
             
--- src/styles/DisplayArea-style.js
+++ src/styles/DisplayArea-style.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 
 const DisplayAreaStyle = styled.div`
     position : absolute;
@@ -13,6 +13,60 @@ const DisplayAreaStyle = styled.div`
     align-items: center;
 `;
 
+/* 이게 왜 안되징
+const calculateRotate = (num) => {
+    return css`
+        transform : rotate(${num})
+    `;
+}
+*/
+
+const TodoDeleteFade = keyframes`
+    0% {
+        transform : scale(1.0) rotate(36deg);
+        
+    }
+    10% {
+        transform : scale(0.9) rotate(72deg);
+   
+    }
+    20% {
+        transform : scale(0.8) rotate(108deg)
+   
+    }
+    30% {
+        transform : scale(0.7) rotate(142deg)
+     
+    }
+    40% {
+        transform : scale(0.6) rotate(178deg)
+ 
+    }
+    50% {
+        transform : scale(0.5) rotate(216deg)
+  
+    }
+    60% {
+        transform : scale(0.4) rotate(252deg)
+
+    }
+    70% {
+        transform : scale(0.3) rotate(280deg)
+;
+    }
+    80% {
+        transform : scale(0.2)  rotate(316deg)
+
+    }
+    90% {
+        transform : scale(0.1) rotate(342deg)
+    }
+    100% {
+        transform : scale(0.1) rotate(360deg)
+    }
+
+`;
+
 const TodoItem = styled.div`
     margin : 0.2rem;
     color : black;
@@ -25,6 +79,13 @@ const TodoItem = styled.div`
     & h2 {
         margin : 0.5rem;
     }
+    ${(props) => {
+        if(props.className === 'deleted'){
+            return css`
+            animation: ${TodoDeleteFade} 3s linear;
+        `}
+        }
+    };
 `;
 
 const DisplayArea = { DisplayAreaStyle, TodoItem }
