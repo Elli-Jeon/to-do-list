4a2864be596e5b13eee4b478dc9697ccd0a134aa
raverena <raverena96@gmail.com>
2021-07-13 15:53:18 +0900

    [Add] calendar area에서 todo 표시

--- src/components/MainCalendar.jsx
+++ src/components/MainCalendar.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useContext } from "react";
+
+// Component
+import Todo from "./Todo";
+
+// Calendar
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
+
+// CSS
+import CalendarAreaStyle from "../styles/CalendarArea-style";
+import { TodoContext } from "../context/todo-context";
+
+const { CalendarArea, SelectedDayTodoArea } = CalendarAreaStyle;
 
 const MainCalendar = () => {
+    const [selectedDay, setSelectedDay] = useState(null);
+    const context = useContext(TodoContext);
+
+    console.log(selectedDay);
+    console.log(context)
     return (
-        <div>
-            Hi!
-        </div>
+        <CalendarArea>
+            <Calendar
+                value={selectedDay}
+                onChange={setSelectedDay}
+                shouldHighlightWeekends
+            />
+            <SelectedDayTodoArea>
+                {context.state.todos.map((eachTodo)=>{
+                    return <Todo key={eachTodo.id} todo={eachTodo} />
+                })}
+            </SelectedDayTodoArea>
+        </CalendarArea>
     )
 }
 
