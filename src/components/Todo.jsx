646dcdff6d3997e9ec0caa2912a7605b0544cb2e
Elli-Jeon <68575268+Elli-Jeon@users.noreply.github.com>
2021-07-14 19:24:28 +0900

    [Fix] useEffect로 삭제 기능 수정 close#7

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,4 @@
-import React , { useState, useContext }from 'react'
+import React , { useState, useContext, useEffect }from 'react'
 import { TodoContext } from '../context/todo-context';
 
 // ACTIONS
@@ -20,13 +20,18 @@ const Todo = ({todo}) => {
         //console.log(e);
         setDeleted(true);
         // setTimeout 안 걸면 바로 상태 변화로 상위 MainTodo에서 map돌 때 삭제되어 버림.
-        setTimeout(()=>{
-            context.dispatch({type : ACTIONS.TODO_DELETE, payload : { todo }});
-            setDeleted(false);
-        },3000)        
+       
     }
 
     // setTimeOut useEffect에 담아야 함.
+    useEffect(()=>{
+        setTimeout(()=>{
+            context.dispatch({type : ACTIONS.TODO_DELETE, payload : { todo }});
+            
+            return () => setDeleted(false); // cleanUp을 함수로 걸어주었기(비동기) 때문에, 사라진 컴포넌트의 deleted를 참조하지 않게 되어서 에러 해결
+        },3000) 
+
+    },[deleted]); // context, todo를 넣어버리면 context, todo가 변하기만 해도 삭제 작업 실행
 
     const todoEdit = (e) => {
         e.preventDefault();
